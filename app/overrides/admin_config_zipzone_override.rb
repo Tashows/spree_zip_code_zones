cc33535c8f6803258f3d6341cd0d4dba649f87f7
Anthony Guidarelli <anthony.guidarelli@gmail.com>
2013-07-22 17:49:09 -0400

    Update config sidebar for spree 2.0

--- app/overrides/admin_config_zipzone_override.rb
+++
@@ -1,7 +0,0 @@
-
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                    :name => "admin_config_zipzone_override",
-                    :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                    :partial => "admin/partials/admin_config",
-                    :disabled => false)
-                  
